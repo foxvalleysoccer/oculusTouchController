09376bf2fe2af158d0a38fa92e09bc652da4b034
Josh Janikowski <janikows@fvtc.edu>
2018-04-30 13:37:55 -0500

    initial commit

---
+++ Assets/Controller_Touch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Touch : MonoBehaviour {
+    public OVRInput.Controller controller;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = OVRInput.GetLocalControllerPosition(controller);
+        
+        transform.rotation = OVRInput.GetLocalControllerRotation(controller);
+    }
+}
